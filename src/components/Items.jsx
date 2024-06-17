c1f29a309df6d6aefc382e864e38eb7fb5731f07
unknown <nemanjadjakovic.b@gmail.com>
2024-06-17 14:36:08 +0200

    Small improvement.

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -8,8 +8,8 @@ const Items = ({ currentItems }) => {
       {currentItems &&
         currentItems.map((mail) => (
           <SingleMailWrapper key={mail.id}>
-            <p>Name:{mail.user.name}</p>
-            <p>Name:{mail.user.name}</p>
+            <p>Subject: {mail.subject}</p>
+            <p>Name: {mail.user.name}</p>
             <ModalTest mail={mail} />
           </SingleMailWrapper>
         ))}
